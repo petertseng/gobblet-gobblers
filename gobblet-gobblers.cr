39b9c853d0a490cc4252b171c94f4f09870aa5f1
Peter Tseng <pht24@cornell.edu>
2017-09-09 19:32:19 -0700

    legal first moves with deduplication
    (break, work not currently planned)

--- gobblet-gobblers.cr
+++ gobblet-gobblers.cr
@@ -1,9 +1,21 @@
 require "./src/board"
 
-b = 129_u64
-GobbletGobblers.print_board(b, GobbletGobblers::RED_GREEN)
+seen = Set(GobbletGobblers::Board).new
 
-GobbletGobblers::TRANSFORMS.each { |t|
-  puts
-  GobbletGobblers.print_board(GobbletGobblers.transform(b, t), GobbletGobblers::RED_GREEN)
+{
+  GobbletGobblers::P1_BIG,
+  GobbletGobblers::P1_MID,
+  GobbletGobblers::P1_SMALL,
+}.each { |piece|
+  (0...GobbletGobblers::SIZE).each { |square|
+    new_board = (piece << (square * GobbletGobblers::BITS_PER_SQUARE))
+    next if seen.includes?(new_board)
+    GobbletGobblers.print_board(new_board, GobbletGobblers::RED_GREEN)
+    seen.add(new_board)
+    GobbletGobblers::TRANSFORMS.each { |t|
+      transformed = GobbletGobblers.transform(new_board, t)
+      seen.add(transformed)
+    }
+    puts
+  }
 }
