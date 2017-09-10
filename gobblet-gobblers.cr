5226c7a623840c6af61ddfeb7fb95a80e603479b
Peter Tseng <pht24@cornell.edu>
2017-09-10 07:19:59 +0000

    move to search

--- gobblet-gobblers.cr
+++ gobblet-gobblers.cr
@@ -1,21 +1,4 @@
 require "./src/board"
+require "./src/search"
 
-seen = Set(GobbletGobblers::Board).new
-
-{
-  GobbletGobblers::P1_BIG,
-  GobbletGobblers::P1_MID,
-  GobbletGobblers::P1_SMALL,
-}.each { |piece|
-  (0...GobbletGobblers::SIZE).each { |square|
-    new_board = (piece << (square * GobbletGobblers::BITS_PER_SQUARE))
-    next if seen.includes?(new_board)
-    GobbletGobblers.print_board(new_board, GobbletGobblers::RED_GREEN)
-    seen.add(new_board)
-    GobbletGobblers::TRANSFORMS.each { |t|
-      transformed = GobbletGobblers.transform(new_board, t)
-      seen.add(transformed)
-    }
-    puts
-  }
-}
+GobbletGobblers.search
