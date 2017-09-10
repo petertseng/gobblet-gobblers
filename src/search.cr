76e7fb4ff9b11ea0ecda0db588204ce6b9e21f01
Peter Tseng <pht24@cornell.edu>
2017-09-10 01:56:00 -0700

    Check moves of existing pieces
    takes 10 seconds now, but seems to report that the win happens in 1
    move, which is obviously incorrect!

--- src/search.cr
+++ src/search.cr
@@ -34,8 +34,11 @@ module GobbletGobblers
 
       opponent = 3 - player_to_move
       heights = (0..SIZE).map { |n| GobbletGobblers.height(board, n) }
-      best_opponent_delay = 0
       to_move_marker = player_to_move << BITS_PER_BOARD
+
+      candidates = [] of Tuple(Board, UInt16)
+
+      # First, check for wins in one move.
 
       spares_present.each { |(piece, spare, height)|
         (0...SIZE).each { |square|
@@ -45,24 +48,63 @@ module GobbletGobblers
           next if @seen.includes?(new_board | to_move_marker)
 
           winners = GobbletGobblers.winners(new_board)
+          raise "Opponent already won, should be impossible?" if winners[opponent - 1]
+
+          return {player_to_move, 1} if winners[player_to_move - 1]
+
+          candidates << {new_board, spares - spare}
+        }
+      }
+
+      (0...SIZE).each { |from_square|
+        next unless GobbletGobblers.owner(board, from_square) == player_to_move
+        from_height = heights[from_square]
+        case {player_to_move, from_height}
+        when {1, 3}; piece = P1_BIG
+        when {1, 2}; piece = P1_MID
+        when {1, 1}; piece = P1_SMALL
+        when {2, 3}; piece = P2_BIG
+        when {2, 2}; piece = P2_MID
+        when {2, 1}; piece = P2_SMALL
+        else raise "Invalid piece #{player_to_move} #{from_height}"
+        end
+        board_without = board & ~(piece << (from_square * BITS_PER_SQUARE))
+
+        (0...SIZE).each { |to_square|
+          next if from_square == to_square
+          next if heights[to_square] >= from_height
+
+          new_board = board_without | (piece << (to_square * BITS_PER_SQUARE))
+          next if @seen.includes?(new_board | to_move_marker)
+
+          winners = GobbletGobblers.winners(new_board)
+
           # My opponent won on this move, don't bother making it
           next if winners[opponent - 1]
 
           return {player_to_move, 1} if winners[player_to_move - 1]
 
-          @seen.add(new_board | to_move_marker)
-          TRANSFORMS.each { |t|
-            transformed = GobbletGobblers.transform(new_board, t)
-            @seen.add(transformed | to_move_marker)
-          }
-
-          sub_winner, sub_turns = winner(new_board, opponent, spares - spare)
-          if sub_winner == player_to_move
-            return {sub_winner, sub_turns + 1}
-          else
-            best_opponent_delay = {best_opponent_delay, sub_turns + 1}.max
-          end
+          candidates << {new_board, spares}
         }
+      }
+
+      candidates.each { |new_board, spares|
+        @seen.add(new_board | to_move_marker)
+        TRANSFORMS.each { |t|
+          transformed = GobbletGobblers.transform(new_board, t)
+          @seen.add(transformed | to_move_marker)
+        }
+      }
+
+      best_opponent_delay = 0
+
+      candidates.each { |new_board, spares|
+        sub_winner, sub_turns = winner(new_board, opponent, spares)
+        if sub_winner == player_to_move
+          return {sub_winner, sub_turns + 1}
+        else
+          best_opponent_delay = {best_opponent_delay, sub_turns + 1}.max
+        end
       }
 
       # I did not win, so my opponent does.
