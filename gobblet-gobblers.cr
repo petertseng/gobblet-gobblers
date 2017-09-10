7acf4cd9a7f430035e110945dbd1fe1c79e6214a
Peter Tseng <pht24@cornell.edu>
2017-09-10 20:24:31 +0000

    allow all moves on command line

--- gobblet-gobblers.cr
+++ gobblet-gobblers.cr
@@ -1,4 +1,8 @@
 require "./src/board"
 require "./src/search"
 
-GobbletGobblers.search(ARGV[0].to_u64, ARGV[1].to_i)
+if ARGV.size >= 2
+  GobbletGobblers.search(ARGV[0].to_u64, ARGV[1].to_i)
+else
+  GobbletGobblers::Search.new.all_moves
+end
