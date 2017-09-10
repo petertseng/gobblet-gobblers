29ff92ab94e899c95f0f666a386d976c66a0126d
Peter Tseng <pht24@cornell.edu>
2017-09-10 00:46:46 -0700

    respect current height

--- src/search.cr
+++ src/search.cr
@@ -17,24 +17,27 @@ module GobbletGobblers
     STARTING_SPARES = P1_SPARE_BIG * 2 + P1_SPARE_MID * 2 + P1_SPARE_SMALL * 2 + P2_SPARE_BIG * 2 + P2_SPARE_MID * 2 + P2_SPARE_SMALL * 2
 
     def search(board : Board = 0_u64, player_to_move : Int32 = 1, spares : UInt16 = STARTING_SPARES)
-      spares_present = [] of Tuple(UInt64, Int32)
+      spares_present = [] of Tuple(UInt64, UInt16, Int32)
 
       case player_to_move
       when 1
-        spares_present << {P1_BIG, 3} if spares & P1_HAS_SPARE_BIG != 0
-        spares_present << {P1_MID, 2} if spares & P1_HAS_SPARE_MID != 0
-        spares_present << {P1_SMALL, 1} if spares & P1_HAS_SPARE_SMALL != 0
+        spares_present << {P1_BIG, P1_SPARE_BIG, 3} if spares & P1_HAS_SPARE_BIG != 0
+        spares_present << {P1_MID, P1_SPARE_MID, 2} if spares & P1_HAS_SPARE_MID != 0
+        spares_present << {P1_SMALL, P1_SPARE_SMALL, 1} if spares & P1_HAS_SPARE_SMALL != 0
       when 2
-        spares_present << {P2_BIG, 3} if spares & P2_HAS_SPARE_BIG != 0
-        spares_present << {P2_MID, 2} if spares & P2_HAS_SPARE_MID != 0
-        spares_present << {P2_SMALL, 1} if spares & P2_HAS_SPARE_SMALL != 0
+        spares_present << {P2_BIG, P2_SPARE_BIG, 3} if spares & P2_HAS_SPARE_BIG != 0
+        spares_present << {P2_MID, P2_SPARE_MID, 2} if spares & P2_HAS_SPARE_MID != 0
+        spares_present << {P2_SMALL, P2_SPARE_SMALL, 1} if spares & P2_HAS_SPARE_SMALL != 0
       else
         raise "Unknown player #{player_to_move}"
       end
 
-      spares_present.each { |(piece, height)|
+      spares_present.each { |(piece, spare, height)|
         (0...SIZE).each { |square|
-          new_board = (piece << (square * BITS_PER_SQUARE))
+          cur_height = GobbletGobblers.height(board, square)
+          next if cur_height >= height
+
+          new_board = board | (piece << (square * BITS_PER_SQUARE))
           next if @seen.includes?(new_board)
 
           # Extra space to align with moves (a1 - c3)
